714f84853850eb95185807053d85ac46a981c975
Quentin384 <zampieri.quentin38@gmail.com>
2025-07-21 11:51:09 +0200

    fix small bug

--- src/main/java/com/example/backendgroupgenerateur/service/UserObjectService.java
+++ src/main/java/com/example/backendgroupgenerateur/service/UserObjectService.java
@@ -3,32 +3,32 @@ package com.example.backendgroupgenerateur.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.example.backendgroupgenerateur.model.UserObject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.backendgroupgenerateur.model.UserObject;
 import com.example.backendgroupgenerateur.repository.UserObjectRepository;
 
 @Service
 public class UserObjectService {
 
-    private final UserObjectRepository UserObjectRepository;
+    private final UserObjectRepository userObjectRepository;
 
     @Autowired
-    public UserObjectService(UserObjectRepository UserObjectRepository) {
-        this.UserObjectRepository = UserObjectRepository;
+    public UserObjectService(UserObjectRepository userObjectRepository) {
+        this.userObjectRepository = userObjectRepository;
     }
 
-    public UserObject create(UserObject UserObject) {
-        return UserObjectRepository.save(UserObject);
+    public UserObject create(UserObject userObject) {
+        return userObjectRepository.save(userObject);
     }
 
     public Optional<UserObject> findById(Long id) {
-        return UserObjectRepository.findById(id);
+        return userObjectRepository.findById(id);
     }
 
     public List<UserObject> findByUserId(Long userId) {
-        return UserObjectRepository.findByUserId(userId);
+        return userObjectRepository.findByOwnerId(userId);
     }
 }
+
